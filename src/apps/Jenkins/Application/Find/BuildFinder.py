c1965adc347165051b72627058531a14916d9061
Stefan Besler <stefan@besler.me>
2021-07-31 21:01:05 +0200

    Console output of jenkins is now polled and echoed on github

--- src/apps/Jenkins/Application/Find/BuildFinder.py
+++ src/apps/Jenkins/Application/Find/BuildFinder.py
@@ -3,11 +3,21 @@ from typing import Type
 
 
 class BuildFinder:
-    def __init__(self, repository: Type[ServerRepository]) -> None:
+    def __init__(self, repository: Type[ServerRepository], name: str) -> None:
         self.repository = repository
+        self.name = name
+        self.consoleLines = 0
 
     def exec(self, number: int) -> str:
-        return self.repository.get_status(number)
 
+        consoleLines = self.repository.get_build_console_output(self.name, number).split("\n")
+        consoleLinesCount = len(consoleLines)
+        for i in range(self.consoleLines, consoleLinesCount):
+            print(consoleLines[i])
+
+        self.consoleLines = consoleLinesCount
+
+        return self.repository.get_status(number)
+    
     def number(self) -> int:
-        return self.repository.get_number()
+        return self.repository.get_number()    
